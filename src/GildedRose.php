e7b023b37698134245e5227931faf1144a4b4f23
Nicholas shiell <nicholas@nshiell.com>
2022-03-21 16:54:54 +0000

    Removing old code and adding in test and code for unknown input type

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -6,11 +6,26 @@ use App\ItemTypes;
 class GildedRose
 {
     private $items;
+    private $itemTypes = [];
 
-    public function __construct(array $items)
+    public function __construct(array $items, array $itemTypes = null)
     {
         $this->items = $items;
+
+        if ($itemTypes === null) {
+            $itemTypes = [
+                new ItemTypes\NormalItemType,
+                new ItemTypes\AgedBrieItemType,
+                new ItemTypes\BackstageItemType,
+                new ItemTypes\ConjuredItemType,
+                new ItemTypes\NormalItemType,
+                new ItemTypes\SulfurasItemType,
+            ];
+        }
+
+        $this->itemTypes = $itemTypes;
     }
+
 
     public function getItem($which = null)
     {
@@ -23,97 +38,18 @@ class GildedRose
     public function nextDay()
     {
         foreach ($this->items as $item) {
-            /** @todo refactor */
-            if ($item->name == 'normal') {
-                $type = new ItemTypes\NormalItemType();
-                $type->nextDay($item);
-                break;
-            }
-
-            if ($item->name == 'Aged Brie') {
-                $type = new ItemTypes\AgedBrielItemType();
-                $type->nextDay($item);
-                break;
-            }
-
-            if ($item->name == 'Sulfuras, Hand of Ragnaros') {
-                $type = new ItemTypes\SulfuraslItemType();
-                $type->nextDay($item);
-                break;
-            }
-
-            if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                $type = new ItemTypes\BackstagelItemType();
-                $type->nextDay($item);
-                break;
-            }
-
-            if ($item->name == 'Conjured Mana Cake') {
-                $type = new ItemTypes\ConjuredItemType();
-                $type->nextDay($item);
-                break;
-            }
-
-            if ($item->name == 'Conjured Mana Cake') {
-                $qualityDayDelata = 2;
-                if ($item->quality - $qualityDayDelata < 0) {
-                    $qualityDayDelata = 0;
-                } elseif ($item->sellIn < 1) {
-                    $qualityDayDelata = 4;
-                }
-
-                $item->quality = $item->quality - $qualityDayDelata;
-                $item->sellIn--;
-
-                if ($item->quality < 0) {
-                    $item->quality = 0;
-                }
-
-                continue;
-            }
-
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
+            $notFound = true;
+            foreach ($this->itemTypes as $type) {
+                if ($type->itemIsThisType($item)) {
+                    $type->nextDay($item);
+                    $notFound = false;
+                    break;
                 }
             }
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+
+            if ($notFound) {
+                throw new \UnexpectedValueException(
+                    sprintf('ItemType for %s was not found', $item));
             }
         }
     }
--- src/ItemTypes/AbstractItemType.php
+++ src/ItemTypes/AbstractItemType.php
@@ -6,7 +6,14 @@ use App\Item;
 
 abstract class AbstractItemType
 {
+    /**
+     * Return trye if thes class can handle the $item
+     */
     abstract public function itemIsThisType(Item $item): bool;
 
+    /**
+     * Some Types don't actually increase or decrease quality or sellIn
+     * Otherwise replace extend this method as needed
+     */
     public function nextDay(Item $item) {}
 }
