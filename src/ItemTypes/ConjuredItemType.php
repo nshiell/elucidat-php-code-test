aa9ab55ec0e8641172c2afaa33e9aa0b43d2178e
Nicholas shiell <nicholas@nshiell.com>
2022-03-21 16:19:55 +0000

    New class ConjuredItemType()

---
+++ src/ItemTypes/ConjuredItemType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\ItemTypes;
+
+use App\Item;
+
+class ConjuredItemType
+{
+    public function nextDay(Item $item)
+    {
+        $qualityDelata = -2;
+        $sellInDelata = -1;
+
+        if ($item->sellIn < 1) {
+            $qualityDelata = -4;
+        }
+
+        // The Quality of an item is never negative
+        if ($item->quality + $qualityDelata < 0) {
+            $qualityDelata = 0;
+        }
+
+        $item->quality = $item->quality + $qualityDelata;
+        $item->sellIn = $item->sellIn + $sellInDelata;
+    }
+}
