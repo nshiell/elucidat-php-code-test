53dbe2424927ec3ebc9aa159417bd23fd219deba
Nicholas shiell <nicholas@nshiell.com>
2022-03-21 16:09:45 +0000

    Quality != 5 0 anbd handle close to event increases BackstagelItemType()

--- src/ItemTypes/BackstagelItemType.php
+++ src/ItemTypes/BackstagelItemType.php
@@ -11,9 +11,17 @@ class BackstagelItemType
         $qualityDelata = 1;
         $sellInDelata = -1;
 
-        // Quality increases by 2 when there are 10 days or less
-        if ($item->sellIn <= 10) {
+        if ($item->sellIn <= 5) {
+            // and by 3 when there are 5 days or less
+            $qualityDelata = 3;
+        } elseif ($item->sellIn <= 10) {
+            // Quality increases by 2 when there are 10 days or less
             $qualityDelata = 2;
+        }
+
+        // The Quality of an item is never more than 50
+        if ($item->quality + $qualityDelata > 50) {
+            $qualityDelata = 50 - $item->quality;
         }
 
         $item->quality = $item->quality + $qualityDelata;
