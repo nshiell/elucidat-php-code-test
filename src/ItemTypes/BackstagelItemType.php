df41b62978a2ef2bebfa4abc2d9d21dda747da8c
Nicholas shiell <nicholas@nshiell.com>
2022-03-21 16:05:59 +0000

    Making 'updates Backstage pass items close to the sell date' work with new class

--- src/ItemTypes/BackstagelItemType.php
+++ src/ItemTypes/BackstagelItemType.php
@@ -11,6 +11,11 @@ class BackstagelItemType
         $qualityDelata = 1;
         $sellInDelata = -1;
 
+        // Quality increases by 2 when there are 10 days or less
+        if ($item->sellIn <= 10) {
+            $qualityDelata = 2;
+        }
+
         $item->quality = $item->quality + $qualityDelata;
         $item->sellIn = $item->sellIn + $sellInDelata;
     }
