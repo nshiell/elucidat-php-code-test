b0f8d42d425b09fe4a4e64b7f632aeda5f850b76
Nicholas shiell <nicholas@nshiell.com>
2022-03-21 15:39:35 +0000

    Making all normal pass new NormalItemType

--- src/ItemTypes/NormalItemType.php
+++ src/ItemTypes/NormalItemType.php
@@ -11,8 +11,14 @@ class NormalItemType
         $qualityDelata = -1;
         $sellInDelata = -1;
 
+        // updates normal items on the sell date
         if ($item->sellIn < 1) {
             $qualityDelata = -2;
+        }
+
+        // The Quality of an item is never negative
+        if ( $item->quality + $qualityDelata < 0) {
+            $qualityDelata = 0;
         }
 
         $item->quality = $item->quality + $qualityDelata;
